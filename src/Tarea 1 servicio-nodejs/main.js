f73dbfe478bf24b0b32d0f8f99a338edb223f6db
RicardoDomi <ricardo.dominguez4666@alumnos.udg.mx>
2025-09-17 00:05:30 -0600

    Update main.js

--- src/Tarea 1 servicio-nodejs/main.js
+++ src/Tarea 1 servicio-nodejs/main.js
@@ -1,13 +1,14 @@
 const users = require('../models/userModel');
-const http = require('http');
-const port = 3000;
+const http  = require('http');
+const port = 3000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req,res)=>{
     if(req.url === '/user' && req.method === 'GET'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(user));
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(users));
     }
 })
+
 server.listen(port,()=>{
-    console.log('Servidor correiendo en puerto '+ port);
+    console.log('servidor corriendo en el puerto '+ port)
 })
