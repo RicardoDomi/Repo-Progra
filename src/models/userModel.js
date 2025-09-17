0a84003dda3d7bb322de450998eb63fd3ba2fd87
RicardoDomi <ricardo.dominguez4666@alumnos.udg.mx>
2025-09-17 00:05:18 -0600

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,15 @@
-const user = [
-  { "username": "Ricardo Dominguez", "age": 20, "password": "passRic_1" },
-  { "username": "Sofía Morales", "age": 24, "password": "s0f1aM0r!" },
-  { "username": "Luis Fernández", "age": 38, "password": "luisF_pass" },
-  { "username": "Mariana Gómez", "age": 21, "password": "mariana_21" },
-  { "username": "Javier Pérez", "age": 45, "password": "j4v1erP3r3z" },
-  { "username": "Camila Soto", "age": 32, "password": "soto_c4m1l4" },
-  { "username": "Andrés Castro", "age": 26, "password": "andres_castro" },
-  { "username": "Valeria Vargas", "age": 37, "password": "vvargas_pass" },
-  { "username": "Sergio Ramos", "age": 30, "password": "s3rg10R4m0s" },
-  { "username": "Gabriela Díaz", "age": 23, "password": "gabiD_23" },
-];
+const {DataTypes} = require('sequelize');
+const sequelize = require('./config/database');
+
+const Modeluser = sequelize.define(
+    'users',{
+        id:{ type : DataTypes.INTEGER,primaryKey:true,autoIncrement:true},
+        username:{type: DataTypes.STRING,allowNull:false},
+        age:{ type: DataTypes.INTEGER},
+        password:{ type: DataTypes.STRING,allowNull:false}
+    },
+    {
+        timestamps:true
+    }
+) 
+module.exports = Modeluser;
